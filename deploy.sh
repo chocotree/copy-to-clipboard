02b718bd1b8d7c1bda8bb23ee91043694fa548bc
IsTree <IsTree@gmail.com>
2020-05-28 16:22:04 +0800

    try deploy sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+mkdir build
+
+cp index.html readme.md -t build
+
+cd build
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:chocotree/copy-to-clipboard.git master:gh-pages
+
+
+cd ..
+rm -rf build
